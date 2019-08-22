5ae5e34bba48e024b69114d18617d8da075f86f6
Do Trung Dung <dotrungdung@vnpt.vn>
2019-08-22 10:52:39 +0700

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+const gulp = require('gulp');
+const zip = require('gulp-zip');
+const { series,parallel } = require('gulp');
+//const del  = require('del');
+const rename = require('gulp-rename');
+var clean = require('gulp-clean');
+
+function compressPackage() {
+	return gulp.src([
+		'../builds/booking-core/**',
+		'../builds/booking-core/**/.**',
+	])
+		.pipe(zip('booking-core.zip'))
+		.pipe(gulp.dest('../builds/'));
+
+}
+function copyPackage() {
+	//del('../builds/booking-core/*/**',{force:true});
+
+	return gulp.src([
+		'**',
+		'**/.*',
+		'!.env',
+		'!node_modules/**',
+		'!.git/**',
+		'!.idea/**',
+		'!storage/installed',
+		'!storage/app/public/**',
+		'!public/storage/**',
+		'!storage/debugbar/**/*.json',
+		'!storage/logs/**/*.log',
+		'!storage/framework/cache/data/**'
+	])
+		.pipe(gulp.dest('../builds/booking-core'));
+}
+function makeEnv() {
+	return gulp.src([
+		'.env.example',
+	])
+		.pipe(rename('.env'))
+		.pipe(gulp.dest('../builds/booking-core'));
+}
+
+function backend(cb) {
+	cb();
+}
+function frontend(cb) {
+	cb();
+}
+
+function cleanBuildFolder(){
+	return gulp.src('../builds/booking-core', {read: false})
+.pipe(clean({force: true}));
+}
+exports.default = series(parallel(backend,frontend),cleanBuildFolder,copyPackage,compressPackage);
+exports.test = series(parallel(backend,frontend),cleanBuildFolder,copyPackage);
+exports.zip = series(compressPackage);
---
+++ modules/Vendor/Models/VendorRequest.php
@@ -0,0 +1,36 @@
+<?php
+namespace Modules\Vendor\Models;
+
+use App\BaseModel;
+use App\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Modules\Core\Models\SEO;
+use Spatie\Permission\Models\Role;
+
+class VendorRequest extends BaseModel
+{
+    use SoftDeletes;
+    protected $table = 'user_upgrade_request';
+    protected $fillable = [
+        'user_id',
+        'approved_time',
+        'role_request',
+        'approved_by',
+        'status',
+    ];
+
+    public static function getModelName()
+    {
+        return __("User upgrade request");
+    }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function approvedBy(){
+        return $this->belongsTo(User::class,'approved_by');
+    }
+    public function role(){
+        return $this->belongsTo(Role::class,'role_request');
+    }
+}
