416af85b366cb9c8a1b8393eb06e957b3966c430
doquangquan93 <doquangquan93@gmail.com>
2020-03-17 16:28:23 +0700

    update newsd 2

--- modules/News/Routes/admin.php
+++ modules/News/Routes/admin.php
@@ -7,6 +7,8 @@
  */
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', 'NewsController@index')->name('news.admin.index');
+
 Route::get('/edit/{id}', 'NewsController@edit')->name('news.admin.edit');
 
 Route::post('/store/{id}','NewsController@store')->name('news.admin.store');
