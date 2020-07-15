9d6642b895a3f1cf8ee27f9cbe47bb0080455212
phucnguyenbn <dranknight@gmail.com>
2020-07-15 09:15:15 +0700

    no message

--- public/module/product/js/product-detail.js
+++ public/module/product/js/product-detail.js
@@ -2,7 +2,7 @@ $(window).on('load',function () {
     let width = $(window).width();
     let gallery_box = $('.product-detail-gallery');
     if(gallery_box.length > 0){
-        let slider = (width < 768) ? {animation:"slide",} : {animation:"slide", controlNav: "thumbnails",};
+        let slider = (width < 768) ? {animation:"slide"} : {animation:"slide", controlNav: "thumbnails"};
         gallery_box.flexslider(slider);
     }
 })
