aa400b5fe0ba50ce1710a52b2b6ce5fb0a82c54f
phucnguyenbn <dranknight@gmail.com>
2020-08-10 09:38:31 +0700

    update...

--- modules/Booking/Views/frontend/detail.blade.php
+++ modules/Booking/Views/frontend/detail.blade.php
@@ -13,15 +13,15 @@
                             <p class="line1"><span>{{$booking->first_name}},</span>
                                 {{__('your order was submitted successfully!')}}
                             </p>
-                        <p class="line2">{{__('Booking details has been sent to:')}} <span>{{$booking->email}}</span></p>
+                        <p class="line2">{{__('Order details has been sent to:')}} <span>{{$booking->email}}</span></p>
                         </div>
 
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-4">
                     <ul class="booking-info-detail">
-                        <li><span>{{__('Booking Number')}}:</span> {{$booking->id}}</li>
-                        <li><span>{{__('Booking Date')}}:</span> {{display_date($booking->created_at)}}</li>
+                        <li><span>{{__('Order Number')}}:</span> {{$booking->id}}</li>
+                        <li><span>{{__('Order Date')}}:</span> {{display_date($booking->created_at)}}</li>
                         @if(!empty($gateway))
                         <li><span>{{__('Payment Method')}}:</span> {{$gateway->name}}</li>
                         @endif
