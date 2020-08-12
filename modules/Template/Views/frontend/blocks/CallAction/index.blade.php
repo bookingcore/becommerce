e9a028aa20b76b0e9627278312420543ecc4be4a
phucnguyenbn <dranknight@gmail.com>
2020-08-12 14:53:29 +0700

    no message

--- modules/Template/Views/frontend/blocks/CallAction/index.blade.php
+++ modules/Template/Views/frontend/blocks/CallAction/index.blade.php
@@ -1,10 +1,10 @@
 <div class="bravo_callAaction" style="background: url({{ get_file_url($bg, 'full') }}) no-repeat">
     <div class="container">
-        <h2 style="font-size: 48px;color: #ffffff;line-height: 1.1;text-align: center" class="vc_custom_heading semibold vc_custom_1513592080860">
+        <h2 style="font-size: 48px;color: #ffffff;line-height: 1.1;text-align: center" class="vc_custom_heading semibold">
             {!! $title !!}
         </h2>
         <div class="martfury-button text-center size-large color-dark">
-            <a href="{{ $link_button }}">Start Selling</a>
+            <a href="#register" data-toggle="modal" data-target="#register">{{__('Start Selling')}}</a>
         </div>
     </div>
 </div>
