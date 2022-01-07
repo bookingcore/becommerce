69a04bf81a9e517ede91cdd6085f40c22f8923c2
Dannie <dannie.haui@gmail.com>
2022-01-07 22:41:17 +0700

    add bs 5

--- themes/Base/layouts/vendor.blade.php
+++ themes/Base/layouts/vendor.blade.php
@@ -7,13 +7,7 @@
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,600,700&amp;amp;subset=latin-ext" rel="stylesheet">
-        <link rel="stylesheet" href="{{ theme_url('Base') }}/plugins/font-awesome/css/font-awesome.min.css">
-        <link rel="stylesheet" href="{{ theme_url('Base') }}/fonts/Linearicons/Linearicons/Font/demo-files/demo.css">
-        <link rel="stylesheet" href="{{ theme_url('Base') }}/plugins/bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="{{ theme_url('Base') }}/plugins/owl-carousel/assets/owl.carousel.css">
-        <link rel="stylesheet" href="{{ theme_url('Base') }}/plugins/select2/dist/css/select2.min.css">
-        <link rel="stylesheet" href="{{ theme_url('Base') }}/vendor/css/style.css">
+        <link rel="stylesheet" href="{{ theme_url('Base') }}/libs/bootstrap/css/boostrap.min.css">
         <link href="{{ asset('libs/flags/css/flag-icon.min.css') }}" rel="stylesheet">
         <link href="{{ theme_url('Base/dist/css/app.css') }}" rel="stylesheet">
         <link href="{{ theme_url('Base/dist/css/vendor.css') }}" rel="stylesheet">
@@ -36,9 +30,9 @@
         @yield('head')
     </head>
     <body class=" {{$body_class ?? ''}}">
-        <main class="ps-main">
+        <main class="bc-main">
             @include('layouts.vendor.sidebar')
-            <div class="ps-main__wrapper">
+            <div class="bc-main__wrapper">
                 @include('layouts.vendor.header')
                 @yield('content')
             </div>
@@ -58,15 +52,10 @@
 
         </script>
 
-        <!-- custom code-->
         <script src="{{ theme_url('Base') }}/js/jquery.min.js"></script>
-        <script src="{{ theme_url('Base') }}/plugins/popper.min.js"></script>
-        <script src="{{ theme_url('Base') }}/plugins/bootstrap/js/bootstrap.min.js"></script>
-        <script src="{{ theme_url('Base') }}/plugins/jquery.matchHeight-min.js"></script>
-        <script src="{{ theme_url('Base') }}/plugins/select2/dist/js/select2.full.min.js"></script>
-        <script src="{{ asset('libs/filerobot-image-editor/filerobot-image-editor.min.js?_ver='.config('app.version')) }}"></script>
+        <script src="{{ theme_url('Base') }}/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
         <script src="{{ asset('libs/bootbox/bootbox.all.min.js') }}"></script>
-        <script src="{{ theme_url('Base') }}/vendor/js/main.js"></script>
+
         <!-- custom scripts-->
         <script  src="{{ theme_url('Base/js/app.js') }}"></script>
         @yield('footer')
