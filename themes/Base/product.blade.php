5583e8078b62e32c3acb82998ab34a0ce93ae44e
Dannie <dannie.haui@gmail.com>
2022-01-08 17:58:23 +0700

    add bs 5

--- themes/Base/product.blade.php
+++ themes/Base/product.blade.php
@@ -4,14 +4,18 @@
     <div class="bc-page--shop" id="shop-sidebar">
         <div class="container">
             <form action="" class="bc_form_filter">
-                <div class="bc-layout--shop">
-                    <div class="bc-layout__left">
+                <div class="row">
+                    <div class="col-md-3">
                         @include("product.sidebar")
                     </div>
-                    <div class="bc-layout__right">
-                        <div class="bc-shopping bc-tab-root">
-                            @include("product.search.header")
-                            @include("product.search.loop")
+                    <div class="col-md-9">
+                        @include("product.search.header")
+                        <div class="row py-3">
+                            @foreach($rows as $row)
+                                <div class="col-sm-4 mb-3">
+                                    @include("product.search.loop")
+                                </div>
+                            @endforeach
                         </div>
                     </div>
                 </div>
