e0a14f6ba379e2f9d9b93b577b1d78a65a8706ef
phucnguyenbn <dranknight@gmail.com>
2022-01-11 09:26:40 +0700

    news update

--- themes/Base/news/sidebar/recent_news.blade.php
+++ themes/Base/news/sidebar/recent_news.blade.php
@@ -8,7 +8,7 @@ if(!count($recent_posts)) return;
         @foreach($recent_posts as $post)
 {{--            @dump($post)--}}
             <?php $translation = $post->translate() ?>
-            <div class="post-content d-flex mb-15">
+            <div class="post-content d-flex mb-3 pb-3">
                 <div class="post-thumb mr-15">
                     <img src="{{ get_file_url($post->image_id) }}" alt="{{$translation->title}}">
                 </div>
