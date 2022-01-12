088931bb39fb65a97ae4555a64072e1214ee6d63
Dannie <dannie.haui@gmail.com>
2022-01-12 14:30:45 +0700

    update product

--- themes/Base/news-detail.blade.php
+++ themes/Base/news-detail.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    <div class="bc-page-blog">
+    <div class="bc-page-blog py-4">
         <div class="container">
             <div class="row">
                 <div class="col-md-8">
