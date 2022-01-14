5f1bbd9b4fa3b3c156e7de8e89100893853f000c
doquangquan93 <doquangquan93@gmail.com>
2022-01-14 13:46:39 +0700

    Update

--- themes/Base/product/filter/attributes.blade.php
+++ themes/Base/product/filter/attributes.blade.php
@@ -5,7 +5,7 @@
     @php
         $translate = $item->translate(app()->getLocale());
     @endphp
-    <div class="mb-3">
+    <div class="mb-3 border-bottom pb-3">
         <h4 class="widget-title fs-22 mb-2">{{$translate->name}}</h4>
         @foreach($item->terms as $key => $term)
             @php $translate = $term->translate(app()->getLocale()); @endphp
--- themes/Base/product/filter/brand.blade.php
+++ themes/Base/product/filter/brand.blade.php
@@ -2,16 +2,18 @@
     @php
         $selected = (array) Request::query('brand');
     @endphp
-    <h4 class="widget-title fs-22 mb-2">{{__("By Brands")}}</h4>
-    <div class="mb-3">
-        @foreach($brands as $item=>$brand)
-            @php $translate = $brand->translate(app()->getLocale()) @endphp
-            <div class="mb-1">
-                <input @if(in_array($brand->id,$selected)) checked @endif type="checkbox" id="brand_{{$brand->id}}" name="brand[]" value="{{$brand->id}}">
-                <label for="brand_{{$brand->id}}">
-                    {{ $translate->name }}
-                </label>
-            </div>
-        @endforeach
+    <div class="mb-3 border-bottom pb-3">
+        <h4 class="widget-title fs-22 mb-2">{{__("By Brands")}}</h4>
+        <div>
+            @foreach($brands as $item=>$brand)
+                @php $translate = $brand->translate(app()->getLocale()) @endphp
+                <div class="mb-1">
+                    <input @if(in_array($brand->id,$selected)) checked @endif type="checkbox" id="brand_{{$brand->id}}" name="brand[]" value="{{$brand->id}}">
+                    <label for="brand_{{$brand->id}}">
+                        {{ $translate->name }}
+                    </label>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endif
