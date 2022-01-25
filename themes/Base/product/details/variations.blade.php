9a91cfbe35aee667ef27061f6c369e9fc98480cf
doquangquan93 <doquangquan93@gmail.com>
2022-01-25 09:05:04 +0700

    Up

--- themes/Base/product/details/variations.blade.php
+++ themes/Base/product/details/variations.blade.php
@@ -21,4 +21,37 @@
             @endforeach
         </div>
     </div>
+
+    <div class="bc-product-variations mb-2">
+        <div class="variation d-flex mb-3">
+            <div class="name me-3 f-w-10">Color: </div>
+            <div class="values d-flex flex-wrap">
+                <div class="item mr-2">
+                    <input class="d-none" type="radio" id="html" name="fav_language" value="HTML">
+                    <label style="background: blue" class="miw-30px mih-30px border rounded d-flex align-items-center pe-2 ps-2 me-2 justify-content-center" for="html"></label>
+                </div>
+                <div class="item mr-2">
+                    <input class="d-none" type="radio" id="html2" name="fav_language" value="HTML2">
+                    <label style="background: red" class="miw-30px mih-30px border rounded d-flex align-items-center pe-2 ps-2 me-2 justify-content-center" for="html2"></label>
+                </div>
+                <div class="item mr-2">
+                    <input class="d-none" type="radio" id="html2" name="fav_language" value="HTML2">
+                    <label style="background: orange" class="miw-30px mih-30px border rounded d-flex align-items-center pe-2 ps-2 me-2 justify-content-center" for="html2"></label>
+                </div>
+            </div>
+        </div>
+        <div class="variation d-flex mb-3">
+            <div class="name me-3 f-w-10">Size: </div>
+            <div class="values d-flex flex-wrap">
+                <div class="item mr-2">
+                    <input class="d-none" type="radio" id="html" name="fav_language" value="HTML">
+                    <label class="miw-30px mih-30px border rounded d-flex align-items-center pe-2 ps-2 me-2 justify-content-center" for="html">S</label>
+                </div>
+                <div class="item mr-2">
+                    <input class="d-none" type="radio" id="html2" name="fav_language" value="HTML2">
+                    <label class="miw-30px mih-30px border rounded d-flex align-items-center pe-2 ps-2 me-2 justify-content-center" for="html2">L</label>
+                </div>
+            </div>
+        </div>
+    </div>
 @endif
