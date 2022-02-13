5bc72d1b35e26c5bef858aad1e973abec9183d26
unknown <nguyentu13696@gmail.com>
2022-02-13 21:39:40 +0700

    add css theme Axtronic

--- themes/Axtronic/layouts/parts/header.blade.php
+++ themes/Axtronic/layouts/parts/header.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="header-wrap">
             <div class="site-branding">
-                a
+                
             </div>
             <div class="site-navigation">
                 @php generate_menu('primary',['class'=>'me-auto mb-2 mb-lg-0']) @endphp
