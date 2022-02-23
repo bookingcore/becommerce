938fd5300c181c10d09abb122d55320f0d0342ca
Kin19 <dinhtien.ql37@gmail.com>
2022-02-24 00:37:43 +0700

    report

--- modules/Report/Views/admin/overview.blade.php
+++ modules/Report/Views/admin/overview.blade.php
@@ -2,27 +2,80 @@
 @section('content')
     <div class="container-fluid">
         <h3 class="mt-2 mt-md-4 mb-4">{{ __("Overview") }}</h3>
-        <form method="GET" action="{{ route('report.admin.overview') }}" class="filter-form filter-form-left d-flex justify-content-start">
-            <div id="reportrange" class="mr-2" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc;">
-                <i class="fa fa-calendar"></i>&nbsp;
-                <span></span> <i class="fa fa-caret-down"></i>
-            </div>
-        </form>
         <hr>
-
         <div class="row mb-4 mt-4">
-            <div class="col-md-2">
-                <div class="panel">
-
-                </div>
-            </div>
-            <div class="col-md-10">
-                <div class="panel">
+            <div class="col-md-12">
+                <div class="panel report-panel">
                     <div class="panel-title d-flex justify-content-between align-items-center">
-                        <strong>{{__('Net sales')}}</strong>
+                        <ul class="select-date-ranger">
+                            <li><a href="#">Year</a></li>
+                            <li><a href="#">Last month</a></li>
+                            <li><a href="#">This month</a></li>
+                            <li class="active"><a href="#">Last 7 days</a></li>
+                            <li>
+                                <form method="GET" action="{{ route('report.admin.overview') }}" class="filter-form filter-form-left d-flex justify-content-start">
+                                    <div id="reportrange" class="mr-2" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc;">
+                                        <i class="fa fa-calendar"></i>&nbsp;
+                                        <span></span> <i class="fa fa-caret-down"></i>
+                                    </div>
+                                    <button class="btn btn-default" type="submit">{{ __("OK") }}</button>
+                                </form>
+
+                            </li>
+                        </ul>
                     </div>
                     <div class="panel-body">
-                        <canvas class="report_chart" data-chart="" ></canvas>
+                        <div class="report-body-tab">
+                            <div class="report-sidebar">
+                                <ul class="chart-list-item">
+                                    <li>
+                                        <div>
+                                            <strong>$0.00</strong>
+                                            <span>gross sales in this period</span>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <strong>$0.00</strong>
+                                            <span>net sales in this period</span>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <strong>0</strong>
+                                            <span>orders placed</span>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <strong>0</strong>
+                                            <span>items purchased</span>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <strong>$0.00</strong>
+                                            <span>refunded 0 orders (0 items)</span>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <strong>$0.00</strong>
+                                            <span>charged for shipping</span>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div>
+                                            <strong>$0.00</strong>
+                                            <span>worth of coupons used</span>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="report-chart">
+                                <canvas class="report_chart" data-chart="" ></canvas>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -95,17 +148,7 @@
             endDate: end,
             "alwaysShowCalendars": true,
             "opens": "right",
-            "showDropdowns": true,
-            ranges: {
-                '{{__("Today")}}': [moment(), moment()],
-                '{{__('This Week')}}': [moment().startOf('week'), end],
-                '{{__("This Month")}}': [moment().startOf('month'), moment().endOf('month')],
-                '{{__("This Year")}}': [moment().startOf('year'), moment().endOf('year')],
-                '{{__("Yesterday")}}': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-                '{{__('Last Week')}}': [moment().subtract(1, 'week').startOf('week'), moment().subtract(1, 'week').endOf('week')],
-                '{{__("Last Month")}}': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
-                '{{__("Last Year")}}': [moment().subtract(1, 'year').startOf('year'), moment().subtract(1, 'year').endOf('year')]
-            }
+            "showDropdowns": true
         }, cb).on('apply.daterangepicker', function (ev, picker) {
             // Reload Earning JS
             console.log(112);
