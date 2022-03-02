5d41e2032f87928d1a5996fdca85fc530f51af10
Kin19 <dinhtien.ql37@gmail.com>
2022-03-03 01:29:23 +0700

    update report

--- modules/Report/Admin/ReportController.php
+++ modules/Report/Admin/ReportController.php
@@ -16,8 +16,42 @@ class ReportController extends AdminController
     public function overview(Request $request){
 
         $this->checkPermission('report_view');
+        $range = $request->get('range', 'last7days');
+        $labels = [];
+        switch ($range){
+            case 'year';
+                $year = date("Y");
+                for ($month = 1; $month <= 12; $month++){
+                    $labels[] = date('F', strtotime($year . "-" . $month . "-01"));
+                }
+                break;
+            case 'last-month';
+                $last_month = Date("m", strtotime("first day of previous month"));
+                for ($i = strtotime(date('Y-'.$last_month.'-0')) + DAY_IN_SECONDS; $i <= strtotime(date('Y-'.$last_month.'-t')); $i += DAY_IN_SECONDS ){
+                    $labels[] = date('d/m', $i);
+                }
+            break;
+            case 'this-month';
+                for ($i = strtotime(date('Y-m-0')) + DAY_IN_SECONDS; $i <= strtotime(date('Y-m-d')); $i += DAY_IN_SECONDS ){
+                    $labels[] = date('d/m', $i);
+                }
+            break;
+            default;
+                for ($i = strtotime('-7 days') + DAY_IN_SECONDS; $i <= strtotime(date('Y-m-d')); $i += DAY_IN_SECONDS ){
+                    $labels[] = date('d/m', $i);
+                }
+            break;
+        }
+
+        $report_chart_data = [
+            'labels' => $labels,
+            'datasets' => [
+                []
+            ]
+        ];
 
         $data = [
+            'report_chart_data' => $report_chart_data,
             'breadcrumbs'        => [
                 [
                     'name'  => __('Overview'),
