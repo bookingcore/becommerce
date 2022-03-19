4ebd8ca2b1b795436a92165c5b236478ade93ace
trant <hatran@suntel.vn>
2022-03-19 11:45:58 +0700

    remove dump

--- themes/Base/order/detail.blade.php
+++ themes/Base/order/detail.blade.php
@@ -71,9 +71,6 @@
                             </thead>
                             <tbody>
                             @foreach($row->items as $orderItem)
-                                <?php
-                                    dump($orderItem);
-                                ;?>
                                 <?php $model = $orderItem->model; ?>
                                 <tr class="cart-item">
                                     <td class="product-name">{{$model ? $model->title : $orderItem->name }} x{{$orderItem->qty}}
