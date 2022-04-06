6abaa53553b5b96dc2c813b48b422c4fe38921e8
Phúc Đỗ <dphuc1808@gmail.com>
2022-04-06 15:22:48 +0700

    add block news

---
+++ themes/Axtronic/Controllers/Blocks/News.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PC
+ * Date: 4/6/2022
+ * Time: 10:48 AM
+ */
+
+namespace Themes\Axtronic\Controllers\Blocks;
+
+use Modules\Template\Blocks\BaseBlock;
+
+class News extends \Modules\Product\Blocks\News
+{
+
+}
--- themes/Axtronic/blocks/banner-slider/index.blade.php
+++ themes/Axtronic/blocks/banner-slider/index.blade.php
@@ -1,35 +1,35 @@
 <!-- Slider main container -->
+@if(!empty($sliders))
 <div class="container">
-
     <div class="banner-slider swiper-container">
         <!-- Additional required wrapper -->
         <div class="swiper-wrapper axtronic-modern-slider ">
             <!-- Slides -->
+            @foreach($sliders as $slide)
             <div class="swiper-slide">
                 <div class="axtronic-box">
                     <div class="axtronic-box-inner">
                         <div class="slide-bg-wrap axtronic-image">
-                            <div class="slide-bg image" style="background-image: url('{{ theme_url('Axtronic/images/mainslider-1.jpg') }}')"></div>
+                            <div class="slide-bg image" style="background-image: url('{{ get_file_url($slide['image']?? false,'full') }}')"></div>
                         </div>
                         <div class="slide-content">
                             <div class="slide-layers">
                                 <div class="title-wrap-line">
                                     <div class="sub-title-wrap">
-                                        <h4 class="sub-title">On Sale</h4>
+                                        <h4 class="sub-title">{{ $slide['sub_title'] }}</h4>
                                     </div>
                                     <div class="title-wrap">
-                                        <h3 class="title">New arrivals Smartphone
-                                        </h3>
+                                        <h3 class="title">{!! clean($slide['title']) !!}</h3>
                                     </div>
                                 </div>
                                 <div class="description-wrap">
                                     <div class="description">
-                                        <p><span >original price $799.00</span><br>$550.99</p>
+                                        <p>{!! clean($slide['sub_text']) !!}</p>
                                     </div>
                                 </div>
                                 <div class="button-wrap">
-                                    <a class="elementor-button" href="#">
-                                        <span class="button-text">Shop Now</span>
+                                    <a class="elementor-button" href="{{ $slide['link_shop_now'] }}">
+                                        <span class="button-text">{{ $slide['btn_shop_now'] }}</span>
                                     </a>
                                 </div>
                             </div>
@@ -37,70 +37,7 @@
                     </div>
                 </div>
             </div>
-            <div class="swiper-slide">
-                <div class="axtronic-box">
-                    <div class="axtronic-box-inner">
-                        <div class="slide-bg-wrap axtronic-image">
-                            <div class="slide-bg image" style="background-image: url('{{ theme_url('Axtronic/images/mainslider-2.jpg') }}')"></div>
-                        </div>
-                        <div class="slide-content">
-                            <div class="slide-layers">
-                                <div class="title-wrap-line">
-                                    <div class="sub-title-wrap">
-                                        <h4 class="sub-title">On Sale</h4>
-                                    </div>
-                                    <div class="title-wrap">
-                                        <h3 class="title">New arrivals Smartphone
-                                        </h3>
-                                    </div>
-                                </div>
-                                <div class="description-wrap">
-                                    <div class="description">
-                                        <p><span>original price $799.00</span><br>$550.99</p>
-                                    </div>
-                                </div>
-                                <div class="button-wrap">
-                                    <a class="elementor-button" href="#">
-                                        <span class="button-text">Shop Now</span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="swiper-slide">
-                <div class="axtronic-box">
-                    <div class="axtronic-box-inner">
-                        <div class="slide-bg-wrap axtronic-image">
-                            <div class="slide-bg image" style="background-image: url('{{ theme_url('Axtronic/images/mainslider-3.jpg') }}')"></div>
-                        </div>
-                        <div class="slide-content">
-                            <div class="slide-layers">
-                                <div class="title-wrap-line">
-                                    <div class="sub-title-wrap">
-                                        <h4 class="sub-title">On Sale</h4>
-                                    </div>
-                                    <div class="title-wrap">
-                                        <h3 class="title">New arrivals Smartphone
-                                        </h3>
-                                    </div>
-                                </div>
-                                <div class="description-wrap">
-                                    <div class="description">
-                                        <p><span>original price $799.00</span><br>$550.99</p>
-                                    </div>
-                                </div>
-                                <div class="button-wrap">
-                                    <a class="elementor-button" href="#">
-                                        <span class="button-text">Shop Now</span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+           @endforeach
         </div>
         <!-- If we need pagination -->
         <div class="swiper-pagination"></div>
@@ -112,3 +49,4 @@
     </div>
 </div>
 
+@endif
