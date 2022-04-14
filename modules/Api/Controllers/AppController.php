1dd46b8219c4bcc356aaed04b69c780ed7571d3e
huytrinh68 <huytrinhuet@gmail.com>
2022-04-15 02:10:42 +0700

    update api get config

--- modules/Api/Controllers/AppController.php
+++ modules/Api/Controllers/AppController.php
@@ -8,12 +8,18 @@ class AppController extends Controller
 {
     public function getConfig(){
         $languages = \Modules\Language\Models\Language::getActive();
+        $categories = \Modules\Product\Models\ProductCategory::getAll();
+        $actives = \App\Currency::getActiveCurrency();
+        $current = \App\Currency::getCurrent('currency_main');
         $res = [
+            'currency' => $actives,
+            'current_currency' => $current,
             'languages'=>$languages->map(function($lang){
                 return $lang->only(['locale','name']);
             }),
+            'categories' => $categories
         ];
-        return $this->sendSuccess($languages);
+        return $this->sendSuccess($res);
     }
 
 }
