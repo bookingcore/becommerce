94a59345a126a1567707e3e6bfc17b424131acac
huytrinh68 <huytrinhuet@gmail.com>
2022-04-19 01:29:49 +0700

    update home api

--- modules/Api/Controllers/AppController.php
+++ modules/Api/Controllers/AppController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Modules\Page\Models\Page;
 use Modules\Template\Models\Template;
 use Modules\Theme\ThemeManager;
-
+use Modules\Media\Models\MediaFile;
 
 class AppController extends Controller
 {
@@ -24,19 +24,44 @@ class AppController extends Controller
             }),
             'categories' => $categories,
             'current_theme' => $current_theme,
-            'page' => $page
         ];
-
         return $this->sendSuccess($res);
     }
 
-    public function getHomeData(Request $request){
-        $q = $request->query('q');
-        $query = Template::query()->select('id', 'title as text');
-        if ($q) {
-            $query->where('title', 'like', '%' . $q . '%');
-        }
+    public function getHomeData(){
+        $query = Template::query()->select('content')->where('id', '=', '2');
         $res = $query->orderBy('id', 'desc')->limit(20)->get();
+        if(!empty($res) && isset($res[0]['content'])) {
+            $res = json_decode($res[0]['content'], true);
+            $mediafile = new MediaFile();
+            foreach ($res as $key => $value) {
+                if($value['type'] == 'banner_slider') {
+                    if(isset($value['model']['sliders']) && !empty($value['model']['sliders'])){
+                        $temp = [];
+                        foreach ($value['model']['sliders'] as $v) {
+                            //(new MediaFile())->findById($id);
+                            $v['image'] = $mediafile->findById($v['image'])['file_path'];
+                            array_push($temp, $v);
+                        }
+                        $value['model']['sliders'] = $temp;
+                        $res[$key] = $value;
+                    }
+                }
+                if($value['type'] == 'promotion') {
+                    if(isset($value['model']['list_items']) && !empty($value['model']['list_items'])){
+                        $temp = [];
+                        foreach ($value['model']['list_items'] as $v) {
+                            //(new MediaFile())->findById($id);
+                            $v['image'] = $mediafile->findById($v['image'])['file_path'];
+                            array_push($temp, $v);
+                        }
+                        $value['model']['list_items'] = $temp;
+                        $res[$key] = $value;
+                    }
+                }
+            }
+        }
+
         return response()->json([
             'results' => $res
         ]);
