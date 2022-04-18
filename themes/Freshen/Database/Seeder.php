59d7545a1020f4939f889fedf142d9513f78b116
doquangquan93 <doquangquan93@gmail.com>
2022-04-18 10:37:34 +0700

    Update

--- themes/Freshen/Database/Seeder.php
+++ themes/Freshen/Database/Seeder.php
@@ -1,13 +1,14 @@
 <?php
-
-
 namespace Themes\Freshen\Database;
-
+use Illuminate\Support\Facades\Artisan;
 
 class Seeder extends \Illuminate\Database\Seeder
 {
 
     public function run(){
-
+        Artisan::call('cache:clear');
+        $this->call(MediaFileSeeder::class);
+        $this->call(GeneralSeeder::class);
+        $this->call(ProductSeeder::class);
     }
 }
