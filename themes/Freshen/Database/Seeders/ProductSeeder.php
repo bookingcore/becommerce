84e64e91a9a91788bb945afff1787741a9b80b73
hatran <hatran@suntel.vn>
2022-04-21 16:52:21 +0700

    update

--- themes/Freshen/Database/Seeders/ProductSeeder.php
+++ themes/Freshen/Database/Seeders/ProductSeeder.php
@@ -37,6 +37,7 @@ class ProductSeeder extends Seeder
             'image-3'   =>  DB::table('media_files')->insertGetId( ['file_name' => 'product-gallery-3', 'file_path' => 'freshen/product/ss3.png', 'file_type' => 'image/png', 'file_extension' => 'png']),
             'image-4'   =>  DB::table('media_files')->insertGetId( ['file_name' => 'product-gallery-4', 'file_path' => 'freshen/product/ss4.png', 'file_type' => 'image/png', 'file_extension' => 'png']),
         ];
+        $productTopCarousel  = DB::table('media_files')->insertGetId( ['file_name' => 'bg-top-carousel', 'file_path' => 'freshen/shop-items/slider1.jpg', 'file_type' => 'image/jpg', 'file_extension' => 'jpg']);
         $productName = [
             'Pineapple (Tropical Gold)',
             "Silver Heinz Ketchup",
@@ -64,7 +65,6 @@ class ProductSeeder extends Seeder
 
         Product::factory()
             ->times(count($productName))
-//            ->count(count($productName))
             ->sequence(function ($sequent)use($productName,$productBrand,$productImage,$productGallery){
                 return [
                     'title'=>$productName[$sequent->index],
@@ -74,6 +74,47 @@ class ProductSeeder extends Seeder
                 ];
             })
             ->create();
+//        Setting page search product
+        $settings = [
+            [
+                'name'=>'fs_search_layout',
+                'val'=>'2'
+            ],
+            [
+                'name'=>'fs_search_item_layout',
+                'val'=>'gird'
+            ],
+            [
+                'name'=>'fs_search_top_product_ids',
+                'val'=>Product::where('status','publish')->limit(6)->inRandomOrder()->get('id')->implode('id',',')
+            ],
+            [
+                'name'=>'fs_search_top_category_ids',
+                'val'=>ProductCategory::where('status','publish')->limit(6)->inRandomOrder()->get('id')->implode('id',',')
+            ],
+            [
+                'name'=>'fs_search_top_carousel',
+                'val'=>[
+                    ["title"=>"<span class=\"fwb\">Healthy Food</span><br><span class=\"text-thm fw400\">&amp; Organic Market</span>",
+                        "sub_title"=>"All natural products",
+                        "desc"=>"<span class=\"fwb\">Organic food</span> is food produced by methods that comply with the <br> standards of organic farming.",
+                        "button_text"=>"SHOP NOW",
+                        "button_link"=>"#",
+                        "image_id"=>$productTopCarousel,
+                        "order"=>1
+                    ],
+                    ["title"=>"<span class=\"fwb\">Get fresher food</span><br><span class=\"text-thm fw400\">&amp; every days</span>",
+                        "sub_title"=>"All natural products",
+                        "desc"=>"<span class=\"fwb\">Organic food</span> is food produced by methods that comply with the <br> standards of organic farming.",
+                        "button_text"=>"SHOP NOW",
+                        "button_link"=>"#",
+                        "image_id"=>$productTopCarousel,
+                        "order"=>2
+                    ]
+                ]
+            ]
+        ];
+        setting_update_items($settings);
 
     }
 
