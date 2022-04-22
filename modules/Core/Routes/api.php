037ff4f17582e4ae37aa922024eeca295f0cf0e1
Dannie <dannie.haui@gmail.com>
2022-04-22 20:37:54 +0700

    add sale count

--- modules/Core/Routes/api.php
+++ modules/Core/Routes/api.php
@@ -3,4 +3,6 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix'=>'v1'],function(){
    Route::get('/configs','V1\AppController@configs')->name('core.api.configs');
    Route::get('/layout','V1\AppController@layout')->name('core.api.layout');
+
+   Route::get('/country','V1\CountryController@index')->name('core.api.country');
 });
