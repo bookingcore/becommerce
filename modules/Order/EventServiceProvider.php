f4422b9b4738324ba448b813493201c9a7d9b3fc
Dannie <dannie.haui@gmail.com>
2022-05-30 21:49:49 +0700

    update

--- modules/Order/EventServiceProvider.php
+++ modules/Order/EventServiceProvider.php
@@ -5,10 +5,8 @@ namespace Modules\Order;
 
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Modules\Order\Events\OrderStatusUpdated;
-use Modules\Order\Events\PaymentUpdated;
+use Modules\Order\Listeners\CouponOrderStatusListener;
 use Modules\Order\Listeners\OrderStatusUpdatedNotification;
-use Modules\Order\Listeners\PaymentUpdatedListener;
-use Modules\Order\Listeners\ProductOnHoldListener;
 use Modules\Order\Listeners\ProductStockListener;
 
 class EventServiceProvider extends ServiceProvider
