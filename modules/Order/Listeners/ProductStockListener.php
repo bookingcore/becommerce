f0e4bcea2bb411d1eafe646e506a265b4717a9bb
doquangquan93 <doquangquan93@gmail.com>
2022-06-09 14:28:27 +0700

    Update Stock

--- modules/Order/Listeners/ProductStockListener.php
+++ modules/Order/Listeners/ProductStockListener.php
@@ -36,36 +36,24 @@ class ProductStockListener
                     $model = $item->model;
                     if(!empty($model) and $model instanceof  Product){
                         if($model->check_manage_stock()){
-                            if(!empty($item->variation_id)){
-                                $variation = $model->variations()->where('id',$item->variation_id)->first();
-                                if(!empty($variation) and $variation->check_manage_stock()){
-                                    $variation->quantity -= $item->qty;
-                                    $variation->sale_count += $item->qty;
-                                    if($variation->quantity <=0){
-                                        $variation->quantity = 0 ;
-                                        $variation->stock_status ='out';
-                                    }
-                                    $variation->save();
-                                }else{
-                                    // using for turn on parent - turn of child
-                                    $model->quantity -= $item->qty;
-                                    $model->sale_count += $item->qty;
-                                    if($model->quantity <=0){
-                                        $model->quantity = 0 ;
-                                        $model->stock_status ='out';
-                                    }
-                                    $model->save();
-                                }
-                            }else{
-                                $model->quantity -= $item->qty;
-                                $model->sale_count += $item->qty;
-                                if($model->quantity <=0){
-                                    $model->quantity = 0 ;
-                                    $model->stock_status ='out';
-                                }
-                                $model->save();
+                            $model->quantity -= $item->qty;
+                            $model->sale_count += $item->qty;
+                            if($model->quantity <=0){
+                                $model->quantity = 0 ;
+                                $model->stock_status ='out';
                             }
-
+                            $model->save();
+                        } elseif (!empty($item->variation_id)){
+                            $variation = $model->variations()->where('id',$item->variation_id)->first();
+                            if(!empty($variation) and $variation->check_manage_stock()){
+                                $variation->quantity -= $item->qty;
+                                $variation->sale_count += $item->qty;
+                                if($variation->quantity <=0){
+                                    $variation->quantity = 0 ;
+                                    $variation->stock_status ='out';
+                                }
+                                $variation->save();
+                            }
                         }
                     }
                     $item->reduced_stock = $item->qty;
@@ -81,27 +69,24 @@ class ProductStockListener
                     $model = $item->model;
                     if(!empty($model) and $model instanceof  Product){
                         if($model->check_manage_stock()){
-                            if(!empty($item->variation_id)){
-                                $variation = $model->variations()->where('id',$item->variation_id)->first();
-                                if(!empty($variation) and $variation->check_manage_stock()){
-                                    $variation->quantity += $item->reduced_stock;
-                                    $variation->sale_count -= $item->qty;
-                                    $variation->sale_count = max(0,$variation->sale_count);
-                                    if($variation->quantity<=0){
-                                        $variation->stock_status ='out';
-                                    }
-                                    $variation->save();
-                                }
-                            }else{
-                                $model->quantity += $item->reduced_stock;
-                                $model->sale_count -= $item->qty;
-                                $model->sale_count = max(0,$model->sale_count);
-                                if($model->quantity<=0){
-                                    $model->stock_status ='out';
-                                }
-                                $model->save();
+                            $model->quantity += $item->reduced_stock;
+                            $model->sale_count -= $item->qty;
+                            $model->sale_count = max(0,$model->sale_count);
+                            if($model->quantity<=0){
+                                $model->stock_status ='out';
                             }
-
+                            $model->save();
+                        } elseif (!empty($item->variation_id)) {
+                            $variation = $model->variations()->where('id',$item->variation_id)->first();
+                            if(!empty($variation) and $variation->check_manage_stock()){
+                                $variation->quantity += $item->reduced_stock;
+                                $variation->sale_count -= $item->qty;
+                                $variation->sale_count = max(0,$variation->sale_count);
+                                if($variation->quantity<=0){
+                                    $variation->stock_status ='out';
+                                }
+                                $variation->save();
+                            }
                         }
                     }
                     $item->reduced_stock = null;
