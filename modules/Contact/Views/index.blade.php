13ba250aedebca98709e67950514d1250a387503
Dannie-BC <dannie.haui@gmail.com>
2022-06-16 13:49:49 +0700

    use stack

--- modules/Contact/Views/index.blade.php
+++ modules/Contact/Views/index.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
-@section('head')
+@push('head')
 	<style type="text/css">
 		.bravo-contact-block .section{
 			padding: 80px 0 !important;
 		}
 	</style>
-@endsection
+@endpush
 @section('content')
 <div id="bravo_content-wrapper">
 	@include("Contact::frontend.blocks.contact.index")
