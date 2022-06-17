2f66a1f7c88fbdeeb3e41b7df2a017aee7426bb0
doquangquan93 <doquangquan93@gmail.com>
2022-06-17 12:32:40 +0700

    Fix layout

--- themes/Freshen/auth/passwords/email.blade.php
+++ themes/Freshen/auth/passwords/email.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center bravo-login-form-page bravo-login-page">
+<div class="container mt-4 mb-5 pt-4 pb-5">
+    <div class="row justify-content-center bravo-reset-pass-form-page ">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Reset Password') }}</div>
@@ -16,7 +16,7 @@
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+                                <input id="email" type="email" class="form-control {{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
@@ -26,7 +26,7 @@
                         </div>
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary mt-3 btn-main-color">
                                     {{ __('Send Password Reset Link') }}
                                 </button>
                             </div>
--- themes/Freshen/auth/passwords/reset.blade.php
+++ themes/Freshen/auth/passwords/reset.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center bravo-login-form-page bravo-login-page">
+    <div class="container mt-4 mb-5 pt-4 pb-5">
+        <div class="row justify-content-center bravo-reset-pass-form-page ">
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">{{ __('Reset Password') }}</div>
@@ -10,13 +10,13 @@
                         <form method="POST" action="{{ route('password.update') }}">
                             @csrf
                             <input type="hidden" name="token" value="{{ request()->route('token') }}">
-                            <div class="form-group row">
+                            <div class="form-group row mb-3">
                                 <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
                                 <div class="col-md-6">
                                     <input id="email" type="email" class="form-control" name="email" value="{{ old('email',$request->email) }}" required autofocus>
                                 </div>
                             </div>
-                            <div class="form-group row">
+                            <div class="form-group row mb-3">
                                 <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
                                 <div class="col-md-6">
                                     <input id="password" type="password" class="form-control" name="password" required>
@@ -30,7 +30,7 @@
                             </div>
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
+                                    <button type="submit" class="btn btn-primary mt-3 btn-main-color">
                                         {{ __('Reset Password') }}
                                     </button>
                                 </div>
