8d28ab57148863c0b2b6cea8477e1f7b7480be8c
Dannie-BC <dannie.haui@gmail.com>
2022-06-24 15:37:30 +0700

    zeomart

---
+++ public/themes/zeomart/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "../../../themes/Zeomart/**/*.blade.php",
+        "./js/**/*.js",
+        "./js/**/*.vue",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
---
+++ public/themes/zeomart/webpack.mix.js
@@ -0,0 +1,12 @@
+const mix = require('laravel-mix');
+
+mix.postCss("css/general.css", "dist/css");
+mix.postCss("css/home.css", "dist/css");
+mix.postCss("css/vendor.css", "dist/css");
+
+mix.options({
+    postCss: [
+        require('postcss-import'),
+        require('tailwindcss')
+    ]
+});
