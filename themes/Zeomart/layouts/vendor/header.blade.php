422a8632e1e85224ac2f7208f39833b04078d4ec
dannie <dannie.haui@gmail.com>
2022-06-27 11:00:02 +0700

    zeomart

--- themes/Zeomart/layouts/vendor/header.blade.php
+++ themes/Zeomart/layouts/vendor/header.blade.php
@@ -37,8 +37,8 @@ $menus = \Modules\Vendor\VendorMenuManager::menus();
             <a href="{{$user->getStoreUrl()}}" title="{{__("View store")}}" class="dropdown-item rounded-[16px] w-[50px] h-[50px] hover:bg-[#F3F5F6] transition duration-200" >
                 <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path></svg>
             </a>
-            @include('layouts.parts.header.notification')
-            @include('layouts.parts.header.user')
+            @include('layouts.vendor.parts.header.notification')
+            @include('layouts.vendor.parts.header.user')
         </ul>
     </div>
 </header>
