92f4fbc955d7b6f65cd9e59d65e5d340c5e59ae9
Kin19 <dinhtien.ql37@gmail.com>
2022-07-04 23:54:16 +0700

    Zeomart : About editor block

--- public/themes/zeomart/tailwind.config.js
+++ public/themes/zeomart/tailwind.config.js
@@ -5,13 +5,6 @@ module.exports = {
         "./js/**/*.vue",
     ],
     theme: {
-        screens: {
-            sm: '640px',
-            md: '768px',
-            lg: '1024px',
-            xl: '1280px',
-            '2xl': '1428px',
-        },
         extend: {
             borderRadius: {
                 '16':'16px'
@@ -20,7 +13,17 @@ module.exports = {
         container: {
             padding: {
                 DEFAULT: '14px'
+            },
+            screens: {
+                sm: '640px',
+                md: '768px',
+                lg: '1024px',
+                xl: '1188px',
+                '2xl': '1428px'
             }
+        },
+        fontSize: {
+            '3xl': ['1.75rem', '2.5rem']
         }
     },
     plugins: [
---
+++ themes/Zeomart/blocks/about-editor/index.blade.php
@@ -0,0 +1,19 @@
+<div class="zm-about-editor mb-10 lg:mb-14">
+    <div class="xl:container mx-auto">
+        @if($title)
+            <h2 class="text-3xl font-medium">{{ $title }}</h2>
+        @endif
+        <div class="mb-2 lg:mb-3">
+            {!! $content !!}
+        </div>
+        <div class="flex flex-wrap">
+            <div class="w-50">
+
+            </div>
+            <div class="w-50">
+
+            </div>
+        </div>
+
+    </div>
+</div>
