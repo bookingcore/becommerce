21f0c7a0a945894b5c873b192e4ba2ddebc6c0a8
hatran <hatran@suntel.vn>
2022-07-06 15:43:52 +0700

    update

--- modules/Report/Routes/admin.php
+++ modules/Report/Routes/admin.php
@@ -5,5 +5,5 @@ Route::get('/overview','ReportController@overview')->name('report.admin.overview
 Route::get('/products','ReportController@products')->name('report.admin.products');
 Route::get('/products/export', 'ReportController@productExport')->name('report.admin.products.export');
 
-//Route::get('/revenue','ReportController@revenue')->name('report.admin.revenue');
+Route::get('/revenue','ReportController@revenue')->name('report.admin.revenue');
 //Route::get('/orders','ReportController@orders')->name('report.admin.orders');
