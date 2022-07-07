afd82d74e3058e1b98cbd670f99aa4dae9b86444
thanhdv <thanhdv193@gmail.com>
2022-07-07 16:48:20 +0700

    update product list

--- themes/Zeomart/product.blade.php
+++ themes/Zeomart/product.blade.php
@@ -3,6 +3,10 @@
     <link href="{{ theme_url('/zeomart/dist/css/product.css') }}" rel="stylesheet">
 @endpush
 @section('content')
+    <?php
+        $layout = request()->query('layout',setting_item('zm_search_layout',1));
+        $listing_list_style = request()->query('list_style',setting_item('zm_search_item_layout'));
+    ?>
      @include('global.breadcrumb')
     <div class="bc-page--shop" id="shop-sidebar">
         <div class="m-auto max-w-7xl">
