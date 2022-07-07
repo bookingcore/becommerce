9df47ac2edda232ab8b4e65ee4b9f5fa8068a0bc
doquangquan93 <doquangquan93@gmail.com>
2022-07-07 14:15:52 +0700

    Update

--- themes/Zeomart/Database/Seeder.php
+++ themes/Zeomart/Database/Seeder.php
@@ -1,9 +1,6 @@
 <?php
-namespace Themes\Freshen\Database;
+namespace Themes\Zeomart\Database;
 use Illuminate\Support\Facades\Artisan;
-use Themes\Freshen\Database\Seeders\ProductAttributeSeeder;
-use Themes\Freshen\Database\Seeders\ProductCategorySeeder;
-use Themes\Freshen\Database\Seeders\ProductSeeder;
 
 class Seeder extends \Illuminate\Database\Seeder
 {
@@ -11,9 +8,5 @@ class Seeder extends \Illuminate\Database\Seeder
     public function run(){
         Artisan::call('cache:clear');
         $this->call(GeneralSeeder::class);
-        $this->call(NewsSeeder::class);
-        $this->call(ProductAttributeSeeder::class);
-        $this->call(ProductCategorySeeder::class);
-        $this->call(ProductSeeder::class);
     }
 }
