7a618ea768e7f65e0bd6d00d6486d11a9624824d
Dannie-BC <dannie.haui@gmail.com>
2022-07-08 11:15:11 +0700

    zeomart

--- public/themes/zeomart/js/_dropdown.js
+++ public/themes/zeomart/js/_dropdown.js
@@ -3,8 +3,13 @@ jQuery(function () {
         var $this = $(this);
         $this.on('click', '.be-dropdown-toggle', function () {
             var container = $(this).closest('.be-dropdown');
-            container.find('.be-dropdown-menu').toggleClass('hidden');
-            container.toggleClass('z-50');
+            if(container.find('.be-dropdown-menu').hasClass('hidden')){
+                container.find('.be-dropdown-menu').removeClass('hidden').show();
+                container.addClass('z-50');
+            }else{
+                container.find('.be-dropdown-menu').addClass('hidden').hide();
+                container.removeClass('z-50');
+            }
             $(document).on('click', function (e) {
                 if (!container.is(e.target) && container.has(e.target).length === 0) {
                     container.find('.be-dropdown-menu').addClass('hidden');
