cd912d79f2a3abab4ee483cee6c5eb5f48660453
doquangquan93 <doquangquan93@gmail.com>
2022-07-08 11:30:47 +0700

    add seed

--- themes/Zeomart/Database/Seeder.php
+++ themes/Zeomart/Database/Seeder.php
@@ -1,6 +1,9 @@
 <?php
 namespace Themes\Zeomart\Database;
 use Illuminate\Support\Facades\Artisan;
+use Themes\Base\Database\Seeders\ProductAttributeSeeder;
+use Themes\Base\Database\Seeders\ProductCategorySeeder;
+use Themes\Base\Database\Seeders\ProductSeeder;
 
 class Seeder extends \Illuminate\Database\Seeder
 {
@@ -8,5 +11,8 @@ class Seeder extends \Illuminate\Database\Seeder
     public function run(){
         Artisan::call('cache:clear');
         $this->call(GeneralSeeder::class);
+        $this->call(ProductAttributeSeeder::class);
+        $this->call(ProductCategorySeeder::class);
+        $this->call(ProductSeeder::class);
     }
 }
