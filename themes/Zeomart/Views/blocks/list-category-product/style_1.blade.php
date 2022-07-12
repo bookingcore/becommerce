edb92043424e4902f41200951fc7dd28eda46600
doquangquan93 <doquangquan93@gmail.com>
2022-07-12 21:05:56 +0700

    Update Block

--- themes/Zeomart/Views/blocks/list-category-product/style_1.blade.php
+++ themes/Zeomart/Views/blocks/list-category-product/style_1.blade.php
@@ -1,5 +1,5 @@
 @if(!empty($categories))
-    @php $id = time() @endphp
+    @php $id = rand() @endphp
     <div class="zm-list-category-product py-10">
         <div class="max-w-7xl m-auto">
             <div class="mb-8 flex justify-between items-center">
@@ -7,18 +7,19 @@
                 <div class="flex" data-tabs-toggle="#zm-tab-toggle-{{ $id }}">
                     @if(!empty($categories))
                         @foreach($categories as $item)
-                            <button data-tabs-target="#zm-tab-{{ $item->id }}" aria-controls="zm-tab-{{ $item->id }}" role="tab" aria-selected="false" class="font-[500] text-base relative pb-1 ml-4 ">
+                            @php if(empty($list_product_cat[ $item->id ]->count())) continue @endphp
+                            <button data-tabs-target="#zm-tab-{{ $item->id.$id }}" aria-controls="zm-tab-{{ $item->id.$id }}" role="tab" aria-selected="false" class="font-[500] text-base relative pb-1 ml-4 ">
                                 {{ $item->translate()->name }}
                             </button>
                         @endforeach
                     @endif
-                    <button data-tabs-target="#zm-tab-all" aria-controls="zm-tab-all" role="tab" aria-selected="false" class="font-[500] text-base relative pb-1 ml-4">
+                    <button data-tabs-target="#zm-tab-all-{{$id}}" aria-controls="zm-tab-all" role="tab" aria-selected="false" class="font-[500] text-base relative pb-1 ml-4">
                         {{ __("All") }}
                     </button>
                 </div>
             </div>
             <div class="bc-content" id="zm-tab-toggle-{{ $id }}">
-                <div class="hidden" id="zm-tab-all"  role="tabpanel">
+                <div class="hidden" id="zm-tab-all-{{$id}}"  role="tabpanel">
                     <div class="bc-carousel owl-theme owl-slider" data-owl-auto="true" data-owl-loop="false" data-owl-speed="5000" data-owl-gap="0" data-owl-nav="true" data-owl-dots="false" data-owl-item="5" data-owl-item-xs="1" data-owl-item-sm="3" data-owl-item-md="3" data-owl-item-lg="4" data-owl-item-xl="5" data-owl-duration="1000" data-owl-mousedrag="on">
                         @foreach($rows as $row)
                             @include('product.search.loop')
@@ -26,7 +27,7 @@
                     </div>
                 </div>
                 @foreach($categories as $item)
-                    <div class="hidden" id="zm-tab-{{ $item->id }}"  role="tabpanel">
+                    <div class="hidden" id="zm-tab-{{ $item->id.$id }}"  role="tabpanel">
                         @php $list_items = $list_product_cat[ $item->id ] ?? [] @endphp
                         <div class="bc-carousel owl-theme owl-slider" data-owl-auto="true" data-owl-loop="false" data-owl-speed="5000" data-owl-gap="0" data-owl-nav="false" data-owl-dots="false" data-owl-item="5" data-owl-item-xs="1" data-owl-item-sm="3" data-owl-item-md="3" data-owl-item-lg="4" data-owl-item-xl="5" data-owl-duration="1000" data-owl-mousedrag="on">
                             @if(!empty($list_items))
--- themes/Zeomart/Views/blocks/list-category-product/style_2.blade.php
+++ themes/Zeomart/Views/blocks/list-category-product/style_2.blade.php
@@ -1,60 +1,49 @@
-<div class="our-shops pb0 pt30 bgc-f5">
-    <div class="container">
-        <div class="fruit_custom_widget">
-            <div class="main-title">
-                <div class="row pl30 pl0-md pr30 pr0-md mb30">
-                    <div class="col-xl-3">
-                        <h3 class="ttc">{{ $title }}</h3>
-                    </div>
-                    <div class="col-xl-9">
-                        <div class="popular_listing_sliders home4_style">
-                            <!-- Nav tabs -->
-                            @if(!empty($categories))
-                                <div class="nav nav-tabs justify-content-end" role="tablist">
-                                    @foreach($categories as $key => $item)
-                                        <button class="nav-link @if($key == 0) active @endif" id="nav-{{ $item->id }}-tab" data-bs-toggle="tab" data-bs-target="#nav-{{ $item->id }}" role="tab" aria-controls="nav-{{ $item->id }}" aria-selected="true">
-                                            <span class="flaticon-harvest mr5"></span> {{ $item->translate()->name }}
-                                        </button>
-                                    @endforeach
-                                </div>
-                            @endif
-                            <!-- Tab panes -->
-                        </div>
-                    </div>
+@if(!empty($categories))
+    @php $id = rand() @endphp
+    <div class="zm-list-category-product py-10">
+        <div class="max-w-7xl m-auto">
+            <div class="mb-8 flex justify-between items-center">
+                <h3 class="font-[500] text-3xl">{{ $title }}</h3>
+                <div class="flex" data-tabs-toggle="#zm-tab-toggle-{{ $id }}">
+                    @if(!empty($categories))
+                        @foreach($categories as $item)
+                            @php if(empty($list_product_cat[ $item->id ]->count())) continue @endphp
+                            <button data-tabs-target="#zm-tab-{{ $item->id.$id }}" aria-controls="zm-tab-{{ $item->id.$id }}" role="tab" aria-selected="false" class="font-[500] text-base relative pb-1 ml-4 ">
+                                {{ $item->translate()->name }}
+                            </button>
+                        @endforeach
+                    @endif
+                    <button data-tabs-target="#zm-tab-all-{{$id}}" aria-controls="zm-tab-all" role="tab" aria-selected="false" class="font-[500] text-base relative pb-1 ml-4">
+                        {{ __("All") }}
+                    </button>
                 </div>
-                <hr>
             </div>
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="popular_listing_sliders home4_style row pb30">
-                        <div class="tab-content col-lg-12" id="nav-tabContent">
-                            <div class="tab-pane fade show active" id="nav-fruit" role="tabpanel" aria-labelledby="nav-fruit-tab">
-                                @if(!empty($rows))
-                                    <div class="feature_product_slider nav_none">
-                                        @foreach($rows as $row)
-                                            <div class="item">
-                                                @include('product.search.loop-3')
-                                            </div>
-                                        @endforeach
-                                    </div>
-                                @endif
+            <div class="bc-content" id="zm-tab-toggle-{{ $id }}">
+                <div class="hidden" id="zm-tab-all-{{$id}}"  role="tabpanel">
+                    <div class="flex flex-wrap ">
+                        @foreach($rows as $row)
+                            <div class="w-1/4 border -ml-[1px] -mb-[1px]">
+                                @include('blocks.list-category-product.loop-style-2')
                             </div>
-                            @foreach($categories as $item)
-                                <div class="tab-pane fade" id="nav-{{ $item->id }}" role="tabpanel" aria-labelledby="nav-{{ $item->id }}-tab">
-                                    @php $list_items = $list_product_cat[ $item->id ] ?? [] @endphp
-                                    @if(!empty($list_items))
-                                        <div class="feature_product_slider nav_none">
-                                            @foreach($list_items as $row)
-                                                @include('product.search.loop-3',['row'=>$row])
-                                            @endforeach
-                                    </div>
-                                    @endif
-                                </div>
-                            @endforeach
-                        </div>
+                        @endforeach
                     </div>
                 </div>
+                @foreach($categories as $item)
+                    <div class="hidden" id="zm-tab-{{ $item->id.$id }}"  role="tabpanel">
+                        <div class="flex flex-wrap">
+                            @php $list_items = $list_product_cat[ $item->id ] ?? [] @endphp
+                            @if(!empty($list_items))
+                                @foreach($list_items as $row)
+                                    <div class="w-1/4 border -ml-[1px] -mb-[1px]">
+                                        @include('blocks.list-category-product.loop-style-2')
+                                    </div>
+                                @endforeach
+                            @endif
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
-</div>
+@endif
+
