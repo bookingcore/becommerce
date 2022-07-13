febafb8dfcfae4a62abaa0e7c1ded47403b3f22b
trant <hatran@suntel.vn>
2022-07-13 09:24:43 +0700

    update

--- modules/Location/Routes/web.php
+++ modules/Location/Routes/web.php
@@ -2,7 +2,7 @@
 use Illuminate\Support\Facades\Route;
 
 // Location
-Route::group(['prefix'=>config('location.location_route_prefix')],function(){
-    Route::get('/{slug}','LocationController@detail')->name("location.detail");;// Detail
-    Route::get('/search/searchForSelect2','LocationController@searchForSelect2')->name("location.searchForSelect");;// Search for select 2
-});
+//Route::group(['prefix'=>config('location.location_route_prefix')],function(){
+//    Route::get('/{slug}','LocationController@detail')->name("location.detail");;// Detail
+//    Route::get('/search/searchForSelect2','LocationController@searchForSelect2')->name("location.searchForSelect");;// Search for select 2
+//});
