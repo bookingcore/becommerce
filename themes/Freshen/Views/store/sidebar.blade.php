77fc0f0d1efaa6c30626aa6bf14436f0ba022bd5
dannie <dannie.haui@gmail.com>
2022-07-13 14:41:40 +0700

    fix template

---
+++ themes/Freshen/Views/store/sidebar.blade.php
@@ -0,0 +1,11 @@
+<div class="d-flex align-items-center border p-3 rounded mb-3">
+    <div class="flex-shrink-0 me-3 circle w-75px h-75px">
+        <img src="{{$user->avatar_url}}" width="75px" alt="{{$user->display_name}}" class="object-cover w-75px h-75px">
+    </div>
+    <div class="flex-grow-1">
+        <strong>{{$user->display_name}}</strong>
+        <p class="mb-0"><a href="mailto:{{ $user->email }}">{{$user->email}}</a></p>
+        <p class="mb-0">{{ __("Member Since :time",["time"=> date("M Y",strtotime($user->created_at))]) }}</p>
+    </div>
+</div>
+@include("product.sidebar")
